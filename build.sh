a72b0c7f2af2394215782ef28dd7b5d05abd292e
Marco Schwarz <marco.swz@hotmail.com>
2025-02-06 19:20:49 +0100

    Testing with cross-compilation

---
+++ build.sh
@@ -0,0 +1,1 @@
+OPENSSL_STATIC=1 cargo build --target x86_64-pc-windows-msvc
--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
         rust-overlay.url = "github:oxalica/rust-overlay";
     };
 
-    outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    outputs = { nixpkgs, flake-utils, rust-overlay,  ... }:
         flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: 
             let
                 pkgs = import nixpkgs {
@@ -16,20 +16,34 @@
                         rust-overlay.overlays.default
                     ];
                 };
+                rust-bin = pkgs.rust-bin.stable.latest.default.override {
+                  extensions = [ "rust-src" ];
+                  targets = [ "x86_64-pc-windows-msvc" ];
+                };
 
             in {
                 devShell = pkgs.mkShell {
+                    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+                    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl ];
+                    OPENSSL_DIR = "${pkgs.openssl.dev}";
+                    nativeBuildInputs = [ pkgs.pkg-config ];
                     buildInputs = with pkgs; [ 
                         cmake
                         rust-analyzer
-                        rust-bin.stable.latest.default
                         pkg-config
                         openssl.dev
+                        openssl
+                        rust-bin
                         rustup
                         libudev-zero
                         vscode-langservers-extracted
                         typescript-language-server
                     ];
+                    shellHook = ''
+                        export PKG_CONFIG_PATH=${pkgs.openssl.dev}/lib/pkgconfig
+                        export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.openssl ]}
+                        export OPENSSL_DIR="${pkgs.openssl.dev}"
+                    '';
                 };
             });
 }
