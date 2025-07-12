9cbe71ab09d09a20644e2b17f393eca187ee4345
Marco Schwarz <marco.swz@hotmail.com>
2025-07-12 11:20:00 +0200

    Add playwright

--- devenv.nix
+++ devenv.nix
@@ -1,16 +1,19 @@
 { pkgs, lib, config, inputs, ... }:
 
 let
-  #openssl = pkgs.openssl.override {
-   #   static = true;
-  #};
+  pkgs-playwright = import inputs.nixpkgs-playwright { system = pkgs.stdenv.system; };
+  browsers = (builtins.fromJSON (builtins.readFile "${pkgs-playwright.playwright-driver}/browsers.json")).browsers;
+  chromium-rev = (builtins.head (builtins.filter (x: x.name == "chromium") browsers)).revision;
 in {
-  env.GREET = "devenv";
-  # env.PKG_CONFIG_PATH = "${pkgs.pkgsStatic.openssl.dev}/lib/pkgconfig";
-  # env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.pkgsStatic.openssl ];
-  # env.OPENSSL_DIR = "${pkgs.pkgsStatic.openssl.dev}";
-  # env.OPENSSL_INCLUDE_DIR = "${pkgs.pkgsStatic.openssl.dev}/include";
-  # env.OPENSSL_LIB_DIR = "${pkgs.lib.getLib pkgs.pkgsStatic.openssl}/lib";
+  env = {
+    PLAYWRIGHT_BROWSERS_PATH = "${pkgs-playwright.playwright.browsers}";
+    PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = true;
+    PLAYWRIGHT_NODEJS_PATH = "${pkgs.nodejs}/bin/node";
+    PLAYWRIGHT_LAUNCH_OPTIONS_EXECUTABLE_PATH = "${pkgs-playwright.playwright.browsers}/chromium-${chromium-rev}/chrome-linux/chrome";
+    GREET = "devenv";
+  };
+    
+  cachix.enable = false;
 
   packages = with pkgs; [
     cmake
@@ -22,7 +25,11 @@ in {
     typescript-language-server
     bashInteractive
     cargo-flamegraph
+    just
+    nodejs
   ];
+
+  languages.javascript.enable = true;
 
   languages.rust = {
     enable = true;
